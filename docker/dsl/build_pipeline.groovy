bd421248984e382eef36ced74bbeba11ea9fa2d2
LiquidGangster <thomasjpkelly@gmail.com>
2019-05-09 10:16:46 -0400

    updated scriptpath to capital 'J' Jenkinsfile

--- docker/dsl/build_pipeline.groovy
+++ docker/dsl/build_pipeline.groovy
@@ -19,7 +19,7 @@ pipelineJob('build-pipeline-job') {
         git { 
           remote { url(repo) } 
           branches('updates-for-solution')
-          scriptPath('jenkinsfile') // The location of the Jenkinsfile that defines the pipeline job within the repo
+          scriptPath('Jenkinsfile') // The location of the Jenkinsfile that defines the pipeline job within the repo
           extensions { }  // required as otherwise it may try to tag the repo, which you may not want 
         } 
       } 
